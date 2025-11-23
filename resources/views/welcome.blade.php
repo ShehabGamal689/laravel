f5fc025699e381439c7121af890d98598c71fbc0
ShehabGamal689 <72823848+ShehabGamal689@users.noreply.github.com>
2025-11-23 08:30:57 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
     <title>Shehab Custom Laravel Page</title>
   </head>
   <body>
-    <h1>WELCOME SHEHAB GAMAL 🚀</h1>
-    <p>If you see this, Nginx + PHP-FPM + Laravel are all using this codebase.</p>
+    <h1>WELCOME SHEHAB GAMAL </h1>
   </body>
 </html>
